66aa9eed6e38bdd6c48864a31f7b2642a35802c4
mzg <15122302865@163.com>
2018-05-04 14:48:01 +0800

    优化登录及注册环节

--- simpledu/forms.py
+++ simpledu/forms.py
@@ -1,5 +1,7 @@
 # coding:utf-8
 
+import re
+
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import Length,Email,EqualTo,Required
@@ -25,6 +27,9 @@ class RegisterForm(FlaskForm):
     def validate_username(self,field):
         if User.query.filter_by(username=field.data).first():
             raise ValidationError('用户名已经存在')
+        if not re.match(r'^[a-zA-Z0-9]{3,24}$',field.data):
+            raise ValidationError('请输入3至24位字母或者数字')
+
     
     def validate_email(self,field):
         if User.query.filter_by(email=field.data).first():
@@ -32,16 +37,17 @@ class RegisterForm(FlaskForm):
 
         
 class LoginForm(FlaskForm):
-    email = StringField('邮箱',validators=[Required(),Email()])
+    #email = StringField('邮箱',validators=[Required(),Email()])
+    username = StringField('Username',validators=[Required(),Length(3,24)])
     password = PasswordField('密码',validators=[Required(),Length(6,24)])
     remember_me = BooleanField('记住我')
     submit = SubmitField('提交')
     
     def validate_email(self,field):
-        if field.data and not User.query.filter_by(email=field.data).first():
-            raise ValidationError('邮箱未注册')
+        if field.data and not User.query.filter_by(username=field.data).first():
+            raise ValidationError('用户名不存在')
 
     def validate_password(self,field):
-        user = User.query.filter_by(email=self.email.data).first()
+        user = User.query.filter_by(username=self.username.data).first()
         if user and not user.check_password(field.data):
             raise ValidationError('密码错误')
